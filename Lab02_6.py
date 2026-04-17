16d78ba525e45485a33d9142f458a2a436d278ef
unknown <0825081.sce@rub.edu.bt>
2026-04-17 15:51:46 +0600

    adding last two digit

--- Lab02_6.py
+++ Lab02_6.py
@@ -1,5 +1,10 @@
-print()
-names_dict = {} 
-std_id1 = (67)
-std_id2 = (81)
-unique_value = (std_id1 + std_id2)%10
+std1_id=input("enter student id:")
+std2_id=input("enter student id:")
+#last two digit std no:
+last_two_1=int(std1_id[-2:])
+last_two_2=int(std2_id[-2:])
+print(last_two_1)
+print(last_two_2)
+#add the last number and divide it by 10 
+unique_value=(last_two_1+last_two_2)%10
+print(f"The unique value = {unique_value}")
