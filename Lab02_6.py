ac1e2dd38e032a49fc9234086f82fda85f7a4b39
unknown <08250267.sce@rub.edu.bt>
2026-04-29 22:58:46 +0600

    adding lab02_06

--- Lab02_6.py
+++ Lab02_6.py
@@ -3,3 +3,56 @@ names_dict = {}
 std_id1 = (67)
 std_id2 = (81)
 unique_value = (std_id1 + std_id2)%10
+
+student = {}
+
+while True:
+    name = input("Enter student name (or type 'exit' to stop): ")
+    if name.lower() == "exit":
+        break
+    if name == "":
+        print("WARNING! No name is entered. Skipping...\n")
+        continue
+    student[name] = 0
+print("\nStudent names in the system: ")
+for name in student:
+    print("-", name)
+print("\n----- Quiz Time! -----\n")
+for name in student:
+    print(f"\nQuiz for {name}: ")
+    score = 0
+
+    ans1 = int(input(f"Question 1: {unique_value} + 2 = "))
+    if ans1 == unique_value + 2:
+        score += 1
+    ans2 = int(input(f"Question 2: {unique_value} * 3 = "))
+    if ans2 == unique_value * 3:
+        score += 1
+    ans3 = int(input(f"Question 3: {unique_value} + 5 = "))
+    if ans3 == unique_value + 5:
+        score += 1
+    
+    student[name] = score
+
+    if score == 3:
+        performance = "Excellent"
+    elif score == 2:
+        performance = "Good"
+    elif score == 1:
+        performance = "Needs Improvement"
+    else:
+        performance = "Poor"
+    
+    if score >= 2:
+        certificate = "Eligible for Certificate."
+    else:
+        certificate = "Not eligible for Certificate."
+    print(f'Score: {score}')
+    print(f'Performance: {performance}')
+    print(f'Certificate Status: {certificate}')
+
+    for i in range(score):
+        print("⭐", end=" ")
+    print("\n")
+print("\n----- Quiz Ended -----")
+print()
